5b4bfd367dd3da3335ae408a1c9e37a7836629bf
Felipe Ayres <felipe.ayres@gruporion.com.br>
2021-06-28 15:35:01 -0300

    ✨ Implementação pronta

--- kudos.js
+++ kudos.js
@@ -20,13 +20,53 @@ const KUDOS_TO_REAL = [
   Recebe: um int representando o número de pontos do usuário
   Retorna: um array contendo os kudos. Ex.: ['OK', 'GOOD'] 
 */
-function getKudosForUser(points) {}
+function getKudosForUser(points) {
+  const KudosArray = [];
+
+  // Garante o ordenamento do vetor de Kudos de maneira decrescente
+  KUDOS_TO_POINTS.sort((a, b) => a.value < b.value ? 1 : -1);
+
+  // Verifica de ordem decrescente a quantidade de Kudos que a pontuação equivale
+  for (i = 0; i < KUDOS_TO_POINTS.length; i++) {
+    // Maior quantidade do Kudo escolhido que é possível pela quantidade de pontos;
+    const KudoQuantity = Math.floor(points / KUDOS_TO_POINTS[i].value);
+    //Restante para ser utilizado com Kudos de valor menor
+    points = points % KUDOS_TO_POINTS[i].value;
+    for (j = 0; j < KudoQuantity; j++)
+      KudosArray.push(KUDOS_TO_POINTS[i].name);
+      // Interromper loop se acabarem os pontos restantes
+      if (points == 0)
+        break
+  }
+
+  return KudosArray;
+
+}
 
 /* 
   Recebe: Recebe um array contendo os nomes dos kudos de um usuário. Ex.: ['OK', 'GOOD']
   Retorna: a mensagem padrão com o valor em reais dos kudos por extenso. Ex.: Parabéns, você ganhou vinte e cinco reais
 */
-function getKudosValueMessageForUser(kudos) {}
+function getKudosValueMessageForUser(kudos) {
+  let money = 0;
+  //Utiliza uma variavel auxiliar para ter os espaços entre as vírgulas inseridos sem alterar a variavel original
+  let kudosAux = Object.assign([],kudos);
+  
+  kudos.map((kudo, index) => {
+    
+    const KudoInformation = KUDOS_TO_REAL.find(a => a.name === kudo);
+    money += KudoInformation.value;
+    // Insere um espaço para igualar a especificação do print
+    if (index != 0) {
+      kudosAux[index] = " " + kudo;
+    }
+  })
+  
+  //Biblioteca utilizada para a escrita completa do numero
+  var writtenNumber = require('written-number');
+  
+  return `Você recebeu ${writtenNumber(money, { lang: 'pt' })} reais em retorno aos kudos ${kudosAux} !`
+}
 
 module.exports = {
   getKudosForUser,
--- kudos.test.js
+++ kudos.test.js
@@ -4,9 +4,19 @@ test('test getKudosForUser', () => {
   expect(kudos.getKudosForUser(30)).toEqual(['GOOD', 'NICE']);
   expect(kudos.getKudosForUser(40)).toEqual(['GOOD', 'GOOD']);
   expect(kudos.getKudosForUser(100)).toEqual(['SUPER']);
+  expect(kudos.getKudosForUser(105)).toEqual(['SUPER','OK']);
+  expect(kudos.getKudosForUser(0)).toEqual([]);
 });
 
 test('test getKudosValueMessageForUser', () => {
-  expect(kudos.getKudosValueMessageForUser(30))
-    .toEqual('Você recebeu treze reais em retorno aos kudos NICE, GOOD!');
+  expect(kudos.getKudosValueMessageForUser(['GOOD', 'NICE']))
+    .toEqual('Você recebeu treze reais em retorno aos kudos GOOD, NICE !');
+  expect(kudos.getKudosValueMessageForUser(['GOOD', 'GOOD']))
+    .toEqual('Você recebeu dezesseis reais em retorno aos kudos GOOD, GOOD !');
+  expect(kudos.getKudosValueMessageForUser(['SUPER']))
+    .toEqual('Você recebeu vinte e cinco reais em retorno aos kudos SUPER !');
+  expect(kudos.getKudosValueMessageForUser(['SUPER', 'OK']))
+    .toEqual('Você recebeu vinte e sete reais em retorno aos kudos SUPER, OK !');
+  expect(kudos.getKudosValueMessageForUser([]))
+    .toEqual('Você recebeu zero reais em retorno aos kudos  !');
 });
